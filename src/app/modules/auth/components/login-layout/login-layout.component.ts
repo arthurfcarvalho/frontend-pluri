9704f2d8ff582b5cde2863f598be776bb8b6acab
arthurfcarvalho <satierfruhtra@protonmail.com>
2024-05-14 15:33:26 -0300

    feat(auth): layout auth pluf002

---
+++ src/app/modules/auth/components/login-layout/login-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-layout.component.html',
+  styleUrl: './login-layout.component.scss'
+})
+export class LoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText = "";
+  @Input() secondaryBtnText = "";
+  @Input() disablePrimaryBtn: boolean = false;
+}
