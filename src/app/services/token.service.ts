84917dc7e95d5bc584f555850c96584fa9c6fb0a
GabrielCamargos28 <gabrielcamargosptc@gmail.com>
2024-05-16 19:31:45 -0300

    feat(token): adicionated tokenService

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+
+const KEY = 'token';
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  constructor() { }
+  saveToken(token: string){
+    console.log('token: ', token)
+    return localStorage.setItem(KEY, token);
+  }
+  deleteToken(){
+    return localStorage.removeItem(KEY);
+  }
+  returnToken(){
+    console.log(KEY)
+    return localStorage.getItem(KEY) ?? "";
+  }
+  hasToken(){
+    return !!this.returnToken();
+  }
 }
