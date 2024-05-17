fae7c08f71f46788af29ed345251cbb3cb93c696
arthurfcarvalho <satierfruhtra@protonmail.com>
2024-05-17 11:20:59 -0300

    refactor(token): adicionado tratamento de exceptions e remoçao de linhas em branco pluf007

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@angular/core';
+import { InvalidTokenError } from 'jwt-decode';
 
-
-const KEY = 'token';
+const AUTH_TOKEN_KEY = 'auth-token';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TokenService {
 
-  saveToken(token: string){
-    return sessionStorage.setItem(KEY, token);
+  saveToken(token: string) {
+    if(!token){
+      throw new InvalidTokenError('Token cannot be null or empty');
+    }
+    
+    try {
+      sessionStorage.setItem(AUTH_TOKEN_KEY, token);
+    } catch (error) {
+      console.error('Error saving token:', error);
+    }
   }
-  deleteToken(){
-    return sessionStorage.removeItem(KEY);
+
+  deleteToken() {
+    try {
+      sessionStorage.removeItem(AUTH_TOKEN_KEY);
+    } catch (error) {
+      console.error('Error deleting token:', error);
+    }
   }
-  returnToken(){
-    return sessionStorage.getItem(KEY) ?? "";
+
+  returnToken(): string | null {
+    return sessionStorage.getItem(AUTH_TOKEN_KEY) ?? null;
   }
+
   hasToken(){
     return !!this.returnToken();
   }
