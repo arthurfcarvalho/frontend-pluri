22a1e83cc0d3cbfa7026fad6e12566879b75552e
arthurfcarvalho <70769410+arthurfcarvalho@users.noreply.github.com>
2024-05-29 17:57:12 -0300

    Revert "Feat imple pluri pluf009"

--- src/app/modules/pluri/models/ApplicationPluriData.model.ts
+++
@@ -1,7 +0,0 @@
-export interface DadosAplicacaoPluri {
-    id: number;
-    data_aplicacao: Date;
-    data_reaplicacao: Date;
-    data_divulgacao_notas: Date;
-    realizado: boolean;
-}
--- src/app/modules/pluri/models/UpdateGenerallnformation.model.ts
+++
@@ -1,7 +0,0 @@
-export interface DadosAtualizarInformacoesGerais {
-    codigo: string;
-    trimestre: number;
-    ano_aplicacao: number;
-    data_inicio_pluri: Date;
-    data_inicio_recuperacao: Date;
-  }
