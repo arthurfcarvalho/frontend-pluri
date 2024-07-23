7b7115a5fe68e2ac033360d03bc68c7bada23e23
arthurfcarvalho <satierfruhtra@protonmail.com>
2024-07-23 16:42:18 -0300

    feat(area): criado pagina de pesquisa do pluri e as rotas para as novas telas pluf031

---
+++ src/app/modules/area/create-area/create-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-area.component.html',
+  styleUrl: './create-area.component.scss'
+})
+export class CreateAreaComponent {
+
+}
