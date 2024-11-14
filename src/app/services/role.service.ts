ad755502378df1a6f0827668c54fa0f341f032a6
arthurfcarvalho <satierfruhtra@protonmail.com>
2024-11-14 14:36:15 -0300

    correcao na rota do api no RoleService

--- src/app/services/role.service.ts
+++ src/app/services/role.service.ts
@@ -10,7 +10,7 @@ import { environment } from '../../environments/environment';
 })
 export class RoleService {
 
-  baseUrl = environment.apiUrl + 'perfil/';
+  baseUrl = environment.apiUrl + '/perfil/';
 
   constructor(private httpClient: HttpClient) { }
 
