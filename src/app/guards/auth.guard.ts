fe1041a3011b7616163b9ae36e6bede2de39b824
Arthur <satierfruhtra@protonmail.com>
2025-01-15 16:13:16 -0300

    organização e comentarios nos guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+
 import { UserService } from '../services/user.service';
 
+/**
+ * Guarda de rota que verifica se o usuário está autenticado antes de permitir o acesso à rota protegida.
+ * Caso o usuário não esteja autenticado, redireciona para a página de login.
+ */
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const router = inject(Router);
--- src/app/guards/perm.guard.ts
+++ src/app/guards/perm.guard.ts
@@ -1,13 +1,28 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { map } from 'rxjs';
+
 import { UserService } from '../services/user.service';
 
+/**
+ * Guarda de rota que verifica se o usuário possui as permissões necessárias antes de permitir o acesso à rota.
+ * Caso o usuário não tenha as permissões necessárias, ele é redirecionado para a página de acesso negado.
+ */
 export const permGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const router = inject(Router);
 
+  // Obtém as permissões exigidas configuradas na rota
   const reqPerm = route.data['perms'];
+
+  /**
+   * Verifica se o usuário possui as permissões necessárias.
+   * - Se o usuário não tiver as permissões:
+   *   - Redireciona para a página de acesso negado (`/acesso-negado`).
+   *   - Retorna `false` para bloquear o acesso à rota.
+   * - Se o usuário tiver as permissões:
+   *   - Retorna `true` para permitir o acesso à rota.
+   */
   return userService.hasPermission(reqPerm).pipe(
     map(hasPermission => {
         if(!hasPermission){
