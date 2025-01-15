a2f04e04d175f2dee40c32ea03ce79b1b3492e9e
Arthur <satierfruhtra@protonmail.com>
2025-01-15 15:52:35 -0300

    organização e comentarios no relatorios.service

--- src/app/services/relatorios.service.ts
+++ src/app/services/relatorios.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -8,10 +9,15 @@ import { environment } from '../../environments/environment';
 })
 export class RelatoriosService {
 
-  baseUrl = environment.apiUrl + '/relatorio/';
+  private readonly baseUrl = `${environment.apiUrl}/relatorio/`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
+  /**
+   * Gera o preview de uma questão.
+   * @param dados Dados necessários para a geração do preview da questão.
+   * @returns Observable contendo o arquivo gerado (formato Blob).
+   */
   previewQuestao(dados: any): Observable<Blob>{
 
     const url = this.baseUrl + 'preview-questao';
@@ -19,18 +25,34 @@ export class RelatoriosService {
     return this.http.post(url, dados,{ responseType: 'blob' });
   }
 
+  /**
+   * Gera o preview de um conjunto de questões a serem enviadas.
+   * @param dados Dados necessários para a geração do preview das questões selecionadas.
+   * @returns Observable contendo o arquivo PDF gerado (formato Blob).
+   */
   previewQuestoesAEnviar(dados: any):Observable<Blob>{
 
     const url = this.baseUrl + 'gerar-pdf-questoes-selecionadas';
 
     return this.http.post(url, dados,{ responseType: 'blob' });
   }
+
+  /**
+   * Faz o download do arquivo em formato TeX com as questões selecionadas.
+   * @param dados Dados necessários para gerar o arquivo TeX.
+   * @returns Observable contendo o arquivo TeX gerado (formato Blob).
+   */
   downloadArquivoTexQuestoes(dados: any): Observable<Blob>{
     const url = this.baseUrl + 'gerar-tex-questoes-selecionadas-download';
 
     return this.http.post(url, dados,{ responseType: 'blob' });
   }
 
+  /**
+   * Gera o preview de uma questão selecionada pelo ID.
+   * @param id ID da questão a ser visualizada.
+   * @returns Observable contendo o arquivo PDF gerado (formato Blob).
+   */
   previewQuestaoSelecionada(id: number):Observable<Blob>{
 
     const url = this.baseUrl + 'gerar-pdf-questao-selecionada';
