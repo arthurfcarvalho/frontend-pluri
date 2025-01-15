dc0be7980f43238baec753d0c481cfd19bcb41d7
Arthur <satierfruhtra@protonmail.com>
2025-01-15 16:00:38 -0300

    organização e comentarios no token.service

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { InvalidTokenError } from 'jwt-decode';
 
-const KEY = 'token';
+const KEY = 'token'; // Chave utilizada para armazenar o token no armazenamento de sessão
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,11 @@ const KEY = 'token';
 
 export class TokenService {
 
+  /**
+   * Salva o token no armazenamento de sessão.
+   * @param token O token a ser salvo.
+   * @throws InvalidTokenError Se o token for nulo ou vazio.
+   */
   saveToken(token: string) {
     if(!token){
       throw new InvalidTokenError('Token cannot be null or empty');
@@ -21,6 +26,9 @@ export class TokenService {
     }
   }
 
+  /**
+   * Remove o token do armazenamento de sessão.
+   */
   deleteToken() {
     try {
       sessionStorage.removeItem(KEY);
@@ -29,10 +37,18 @@ export class TokenService {
     }
   }
 
+  /**
+   * Retorna o token armazenado no armazenamento de sessão.
+   * @returns O token armazenado ou `null` se não existir.
+   */
   returnToken(): string | null {
     return sessionStorage.getItem(KEY) ?? null;
   }
 
+  /**
+   * Verifica se existe um token armazenado.
+   * @returns `true` se houver um token, caso contrário, `false`.
+   */
   hasToken(){
     return !!this.returnToken();
   }
