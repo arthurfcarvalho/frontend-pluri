532e5f1a522f9fb4dd7a48326ce1dca51e28d5a1
GabrielCamargos28 <gabrielcamargosptc@gmail.com>
2025-05-13 15:13:59 -0300

    fix: refactor

--- src/app/modules/professor/pages/criar-questoes/criar-questoes.component.ts
+++ src/app/modules/professor/pages/criar-questoes/criar-questoes.component.ts
@@ -254,7 +254,7 @@ export class CreateQuestionsComponent implements OnInit {
       ? assuntosValue.map((a: any) => a?.id)
       : [assuntosValue?.id];
 
-    if(this.criarQuestaoForm.value.assuntosInterdisciplinares?.length > 0){
+    if(this.criarQuestaoForm.value?.assuntosInterdisciplinares?.length > 0){
       formValue.assuntosInterdisciplinares = this.criarQuestaoForm.value.assuntosInterdisciplinares.map((a: any) => a.id);
     }
     this.criarQuestaoForm.value.disciplinas = Array(this.criarQuestaoForm.value.disciplinas)
