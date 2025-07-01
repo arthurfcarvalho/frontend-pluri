3a24683e75cbb601fa4c9df77d8ab4356c35b2b7
GabrielCamargos28 <gabrielcamargosptc@gmail.com>
2025-07-01 17:00:47 -0300

    refactor prod line redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost',
+    apiUrl: 'http://200.131.116.21:8081',
     featureToggle: false
   };
-
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost',
+    apiUrl: 'http://localhost:8080',
     featureToggle: true
   };
 
