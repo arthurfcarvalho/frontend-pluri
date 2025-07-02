3447978acdb52a376728836fd33c329fb6bdf2cc
alexssander77 <alexssandercastro1@gmail.com>
2025-07-02 17:49:03 -0300

    Adição de edição de perfis

---
+++ src/app/modules/role/pages/edit-role/edit-role.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../../home/components/header/header.component';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { AreaService } from '../../../../services/area.service';
+import { ToastrService } from 'ngx-toastr';
+import { RoleService } from '../../../../services/role.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Role } from '../../../../models/Role.model';
+
+@Component({
+  selector: 'app-edit-role',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FieldsetModule,
+    InputTextModule,
+    FloatLabelModule,
+    DropdownModule,
+    ButtonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './edit-role.component.html',
+  styleUrls: ['./edit-role.component.scss']
+})
+export class EditRoleComponent implements OnInit {
+
+  editRoleForm: FormGroup;
+  areaOptions!: any[];
+  roleId!: number;
+  role: Role = {} as Role;
+
+  constructor(
+    private areaService: AreaService,
+    private toastService: ToastrService,
+    private roleService: RoleService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.editRoleForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      codigo_permissoes: new FormControl([], Validators.required),
+      id_area: new FormControl('', Validators.required)
+    });
+  }
+
+  ngOnInit(): void {
+    this.areaService.returnAllAreas().subscribe(data => {
+      this.areaOptions = data.content;
+
+      this.roleId = Number(this.route.snapshot.paramMap.get('id'));
+
+      if (this.roleId) {
+        this.roleService.getById(this.roleId).subscribe(role => {
+          this.role = role;
+          this.editRoleForm.patchValue({
+            nome: this.role.nome,
+            id_area: this.areaOptions.find(a => a.id === role.area.id) || null
+          });
+        });
+      }
+    });
+  }
+
+  submit() {
+    if (this.editRoleForm.invalid) {
+      this.editRoleForm.markAllAsTouched();
+      return;
+    }
+
+    const formValue = {
+      ...this.editRoleForm.value,
+      id_area: this.editRoleForm.value.id_area.id || this.editRoleForm.value.id_area
+    };
+
+    // this.roleService.update(this.roleId, formValue).subscribe({
+    //   next: () => {
+    //     this.toastService.success('Perfil atualizado com sucesso!');
+    //     this.router.navigate(['/roles']);
+    //   },
+    //   error: () => {
+    //     this.toastService.error('Erro ao atualizar perfil!');
+    //   }
+    // });
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8083',
+    apiUrl: 'http://localhost:8081',
     featureToggle: true
   };
 
